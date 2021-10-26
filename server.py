f837dac4189e8f8cdf4a6e791193bbb228c5ef70
anasvakyathodi <anasvakyathodi@gmail.com>
2021-10-26 08:24:19 +0530

    make it better

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os,requests,time
+from datetime import date
 from dotenv import load_dotenv
 
 # get env variables
@@ -13,12 +14,22 @@ class TelegramBot:
         self.token = token
         self.endpoint = endpoint
         self.last_message = None
+        self.time_table = {
+            'Mon':['Distributed Databases','Research Methodology & IPR','Advanced Computer Architecture','Advanced Algorithm','English for RPW','Intelligent System','Library','Library'],
+            'Tue':['Advanced Computer Architecture','Distributed Databases','Research Methodology & IPR','Advanced Algorithm','English for RPW','Intelligent System','Distributed Databases Lab'],
+            'Wed':['Intelligent System','Advanced Algorithm','Research Methodology & IPR','Distributed Databases','English for RPW','Library','Distributed Databases Lab'],
+            'Thu':['Advanced Computer Architecture','Library','Research Methodology & IPR','Advanced Algorithm','English for RPW','Library'],
+            'Fri':['Library','Advanced Computer Architecture','Distributed Databases','Intelligent System','Advanced Algorithm Lab'],
+            'Sat':['Library'],
+            'Sun':['Holiday']
+        }
 
     # get last message 
     def get_last_message(self):
         url = self.endpoint + 'bot' + self.token + '/getUpdates'
         res = requests.get(url)
-        self.last_message = dict(res.json()['result'][-1])
+        if len(res.json()['result'])>0:
+            self.last_message = dict(res.json()['result'][-1])
         return
 
     # reply dictionary
@@ -34,16 +45,24 @@ class TelegramBot:
         'good morning':'Good Morning',
         'good afternoon':'Good Afternoon',
         'good night':'Good Night',
-        'bye':'See You'
+        'bye':'See You',
+        '/today':'today',
+        '/yesterday':'yesterday',
+        '/this_week':'this_week',
+        '/this_month':'this_month',
+        '/this_year':'this_year',
+        '/time_table':'time_table'
         }
-        return switch.get(msg.lower(),"Sorry I didn't get it!")
+        res = switch.get(msg.lower(),"Sorry I didn't get it!")
+        return res
 
     # send message
     def send_message(self,user_id,message):
         url = self.endpoint + 'bot' + self.token + '/sendMessage'
         payload = {
             'chat_id':user_id,
-            'text':message
+            'text':message,
+            'parse_mode':'HTML'
         }
         requests.post(url,payload)
         return
@@ -62,10 +81,55 @@ class TelegramBot:
                     user_name = self.last_message['message']['from']['first_name']
                     user_id = self.last_message['message']['from']['id']
                     reply = self.message_reply(user_text,user_name)
-                    self.send_message(user_id,reply)
-                    last_update_id += 1
-                    print('{} : {}'.format(user_name,user_text))
-                    print('{} : {}'.format('bot',reply))
+                    if(reply in ['all','today','yesterday','this_week','this_month','this_year']):
+                        data = requests.request('GET','https://anz-academy-server.herokuapp.com/notifications')
+                        data = data.json()['data'][::-1]
+                        for dt in data:       
+                            msg = """<b><i>{date}</i></b>
+                            <b>{message}</b>
+                            Link: <a href="{link}">Click here</a>""".format(date=dt['date'],message=dt['message'],link=dt['link'])
+                            given_date = time.strptime(dt['date'],"%d %B, %Y")
+                            today = date.today()
+                            diff = today - date(given_date.tm_year,given_date.tm_mon,given_date.tm_mday)
+                            if reply == 'today' and diff.days == 0:
+                                self.send_message(user_id,msg)
+                            elif reply == 'yesterday' and diff.days == 1:
+                                self.send_message(user_id,msg)
+                            elif reply == 'this_week' and diff.days <= 7:
+                                self.send_message(user_id,msg)
+                            elif reply == 'this_month' and diff.days <= 30:
+                                self.send_message(user_id,msg)
+                            elif reply == 'this_year' and diff.days <= 365:
+                                self.send_message(user_id,msg)   
+                            time.sleep(1)
+
+                        print('{} : {}'.format(user_name,user_text))
+                        print('{} : {}'.format('bot','data sent!'))
+                        last_update_id += 1                                          
+                    
+                    elif reply == 'time_table':
+                        today = date.today()
+                        today = today.weekday()
+                        days = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+                        msg = """<b>Today's Periods</b>
+
+"""
+                        count = 1
+                        today_schedules = self.time_table[days[today]]
+                        for sub in today_schedules:
+                            msg += """{}- {}
+""".format(count,sub)
+                            count += 1
+                        self.send_message(user_id,msg)
+                        last_update_id += 1
+                        print('{} : {}'.format(user_name,user_text))
+                        print('{} : {}'.format('bot','timetable sent!'))
+
+                    else:
+                        self.send_message(user_id,reply)
+                        last_update_id += 1
+                        print('{} : {}'.format(user_name,user_text))
+                        print('{} : {}'.format('bot',reply))
             else:
                 print('No Message Here')
             time.sleep(1)
